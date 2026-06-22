0d273503c3d96e4f4b969bd07decc758b1d30171
Ashwij Kumar Chiguru <chiguruashwijkumar@gmail.com>
2026-06-22 21:29:36 +0530

    Second Commit

--- src/hooks/useStudiesDashboard.js
+++ src/hooks/useStudiesDashboard.js
@@ -1,25 +1,11 @@
-
-// newly added
-
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 import { getStudiesDashboard } from "../services/dashboardService";
 
 function useStudiesDashboard() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    loadDashboard();
-  }, []);
-
-  const loadDashboard = async () => {
-    const response =
-      await getStudiesDashboard();
-
-    setData(response);
-  };
+  const data = useMemo(() => getStudiesDashboard(), []);
 
   return {
-    data,
+    data
   };
 }
 
